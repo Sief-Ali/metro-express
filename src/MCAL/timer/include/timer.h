2f5894cf36a0a5428bb45289bac99517c859949c
Sief Ali <sief4136@gmail.com>
2026-07-07 15:58:24 +0300

    refactor: Introduce dedicated timer registers module

--- src/MCAL/timer/include/timer.h
+++ src/MCAL/timer/include/timer.h
@@ -7,7 +7,8 @@ typedef enum
 {
     TIMER_0,
     TIMER_1,
-    TIMER_2
+    TIMER_2,
+    TIMER_COUNT
 } timer_id_t;
 
 typedef enum
@@ -17,7 +18,8 @@ typedef enum
     TIMER_PRESCALER_8,
     TIMER_PRESCALER_64,
     TIMER_PRESCALER_256,
-    TIMER_PRESCALER_1024
+    TIMER_PRESCALER_1024,
+    TIMER_PRESCALER_COUNT
 } timer_prescaler_t;
 
 void TIMER_Delay(
