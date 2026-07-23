95b263507aac45d584e4e4ef8c9486e62c9eb8bd
Sief Ali <sief4136@gmail.com>
2026-07-23 18:50:37 +0300

    use my student id

--- src/app/passenger/passenger.c
+++ src/app/passenger/passenger.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#define STUDENT_ID "463688499"
+#define STUDENT_ID "463688431"
 
 /* -------------------------------------------------------------------------- */
 /* Dynamic Passenger Configuration                                             */
